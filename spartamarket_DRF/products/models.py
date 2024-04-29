fcb99039e93abd8500d73d1d498cb6022a87bf9e
pkg0203 <pkg0203@naver.com>
2024-04-29 19:25:15 +0900

    Product 모델 정의 및 migrate

--- spartamarket_DRF/products/models.py
+++ spartamarket_DRF/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Product(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to="media/",blank=False)
