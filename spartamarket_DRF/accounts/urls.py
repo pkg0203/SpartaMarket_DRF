2f0acc4817a1ecf512c1ea471c7794afb09d1c79
pkg0203 <pkg0203@naver.com>
2024-04-29 16:41:05 +0900

    비밀번호 Encode 완료

--- spartamarket_DRF/accounts/urls.py
+++ spartamarket_DRF/accounts/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
 from .views import *
 
 urlpatterns = [
-    path("", AccountSignIn.as_view(), name="Sign_in"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("", AccountSignIn.as_view(), name="sign_in"),
+    path("login/", TokenObtainPairView.as_view(), name="log_in"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- spartamarket_DRF/accounts/views.py
+++ spartamarket_DRF/accounts/views.py
@@ -3,11 +3,13 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import *
-
+from .models import *
 # Create your views here.
 class AccountSignIn(APIView):
-    def post(self,request):
-        serializer=AccountSignInSerializer(data=request.data)
+    def post(self, request):
+        serializer = AccountSignInSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            user = serializer.save() 
+            user.set_password(request.data["password"])  
+            user.save()  
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
