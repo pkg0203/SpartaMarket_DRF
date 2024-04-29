f91debb103efba12fca07042480d0d5de5bca7ae
pkg0203 <pkg0203@naver.com>
2024-04-29 14:18:10 +0900

    setting.py 설정 및 AUTH_USER_MODEL 지정

--- spartamarket_DRF/spartamarket_DRF/settings.py
+++ spartamarket_DRF/spartamarket_DRF/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party
+    'django_seed',
+    'django_extensions',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    # Local
+    'accounts',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +88,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -98,8 +107,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
