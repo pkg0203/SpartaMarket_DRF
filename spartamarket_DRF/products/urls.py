05f20adcec7c3333f2851e4473bef7a6dddd9fa1
pkg0203 <pkg0203@naver.com>
2024-04-30 14:52:52 +0900

    Product delete 구현 완료

--- spartamarket_DRF/products/urls.py
+++ spartamarket_DRF/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("",ProductsView.as_view(), name="log_in")
+    path("",ProductsView.as_view(), name="Product"),
+    path("<int:product_id>/",ProductsDetailView.as_view(), name="Product_Detail")
 ]
--- spartamarket_DRF/products/views.py
+++ spartamarket_DRF/products/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.response import Response 
+from rest_framework.response import Response
 from .serializers import *
 
 # Create your views here.
+
+
 class ProductsView(APIView):
-    def get(self,request):
-        products=Product.objects.all()
-        serializer= ProductSerializer(products,many=True)
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def post(self,request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save(author=request.user)
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+    def post(self, request):
+        if request.user.is_authenticated:
+            serializer = ProductSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(author=request.user)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class ProductsDetailView(APIView):
+    def delete(self, request, product_id):
+        product = get_object_or_404(Product, pk=product_id)
+        if request.user.is_authenticated:
+            if request.user == product.author:
+                product.delete()
+                data = {f"pk : {product_id} is successfully deleted"}
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({"본인의 글이 아닙니다."},status=status.HTTP_403_FORBIDDEN)
+        return Response({"로그인을 해주세요."},status=status.HTTP_401_UNAUTHORIZED)
